5546a2048664a5e4983b591726b86199970f14c9
Flávio Félix <fau_felix30@hotmail.com>
2023-03-08 16:29:02 -0300

    Feito o requisito 3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
+const searchProduct = 'computador';
+
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+async function listagemDeProdutosNaPagina() {
+  const requisicaoProdutos = await fetchProductsList(searchProduct);
+  const secaoProdutos = document.querySelector('.products');
+
+  requisicaoProdutos.forEach((product) => {
+    const ProdutoEl = createProductElement(product);
+    secaoProdutos.appendChild(ProdutoEl);
+  });
+}
+
+listagemDeProdutosNaPagina();
--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -4,22 +4,24 @@ import computadorSearch from './mocks/search';
 
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
+  const API_URL = "https://api.mercadolibre.com/sites/MLB/search?q=";
+  const searchProduct = 'computador';
   it('fetchProductsList é uma função', () => {
     expect(typeof fetchProductsList).toBe('function');
   });
 
   it('fetch é chamado ao executar fetchProductsList', async () => {
-    await fetchProductsList('computador')
+    await fetchProductsList(searchProduct)
       expect(fetch).toHaveBeenCalled();
   });
 
   it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
-    await fetchProductsList('computador')
-      expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+    await fetchProductsList(searchProduct)
+      expect(fetch).toHaveBeenCalledWith(API_URL + searchProduct)
   });
 
    it('Teste se o retorno da função fetchProductList com o argumento computador é uma estrutura de dados igual ao objeto computadorSearch que já está importado no arquivo.', async () => {
-      const data = await fetchProductsList('computador');
+      const data = await fetchProductsList(API_URL);
       expect(data).toEqual(computadorSearch);
     });
     it('Teste se, ao chamar a função fetchProductList sem argumento, retorna um erro com a mensagem: termo de busca não informado', async () => {
