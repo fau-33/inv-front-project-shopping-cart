e3b92284bb6dc242ab274b037c5c2b65876a3996
Flávio Félix <fau_felix30@hotmail.com>
2023-03-08 19:43:26 -0300

    Requisito 4

--- src/main.js
+++ src/main.js
@@ -7,7 +7,16 @@ const searchProduct = 'computador';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
+function criaLoadingDeTexto() {
+  const loaderesSectionElemento = document.querySelector('.container');
+  const loaderesPElemento = document.createElement('p');
+  loaderesPElemento.innerText = 'carregando...';
+  loaderesPElemento.classList.add('loading');
+  loaderesSectionElemento.appendChild(loaderesPElemento);
+}
+
 async function listagemDeProdutosNaPagina() {
+  criaLoadingDeTexto();
   const requisicaoProdutos = await fetchProductsList(searchProduct);
   const secaoProdutos = document.querySelector('.products');
 
