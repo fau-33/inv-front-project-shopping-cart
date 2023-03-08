be6b6d1b109b23553983f4bdc96797c7e309e0d9
Flávio Félix <fau_felix30@hotmail.com>
2023-03-08 15:12:57 -0300

    Feito o requisito 2

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -1,7 +1,16 @@
+const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=';
+
 export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // começando de novo
+export const fetchProductsList = async (searchProduct) => {
+  if (!searchProduct) {
+    throw new Error('Termo de busca não informado');
+  }
+
+  const response = await fetch(API_URL + searchProduct);
+  const data = await response.json();
+
+  return data.results;
 };
