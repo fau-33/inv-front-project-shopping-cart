2fa4032fb1007b53a7e9f5ba8adafdf8a43db7f0
Flávio Félix <fau_felix30@hotmail.com>
2023-03-13 22:18:13 -0300

    Repassando o req 2

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -14,12 +14,8 @@ export const fetchProduct = async (searchProductId) => {
 export const fetchProductsList = async (searchProduct) => {
   if (!searchProduct) throw new Error('Termo de busca não encontrado');
 
-  try {
-    const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=';
-    const response = await fetch(API_URL + searchProduct);
-    const data = await response.json();
-    return data.results;
-  } catch (error) {
-    throw new Error('Algum erro ocorreu, recarregue a página e tente novamente');
-  }
+  const API_URL = 'https://api.mercadolibre.com/sites/MLB/search?q=';
+  const response = await fetch(API_URL + searchProduct);
+  const data = await response.json();
+  return data.results;
 };
