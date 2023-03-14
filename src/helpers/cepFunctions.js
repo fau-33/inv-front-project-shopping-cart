63e3b893e3310685c6a2487243dd553ff184f0cc
Flávio Félix <fau_felix30@hotmail.com>
2023-03-13 21:21:10 -0300

    Feito req 11

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,7 +1,39 @@
-export const getAddress = () => {
-  // seu código aqui
+export const getAddress = async (CEP) => {
+  if (!CEP) {
+    throw new Error('Forneça um CEP');
+  }
+  try {
+    const [dataApi, data] = await Promise.all([
+      fetch(`https://cep.awesomeapi.com.br/json/${CEP}`).then((response) => response.json()),
+      fetch(`https://brasilapi.com.br/api/cep/v2/${CEP}`).then((response) => response.json()),
+    ]);
+    return dataApi ?? data;
+  } catch (error) {
+    throw new Error('Não foi possível obter o endereço para o CEP fornecido.');
+  }
 };
 
-export const searchCep = () => {
-  // seu código aqui
-};
+export async function searchCep() {
+  const cartAddressSpan = document.querySelector('.cart__address');
+  const cepbutton = document.querySelector('.cep-button');
+  cepbutton.addEventListener('click', async function () {
+    const cepInput = document.querySelector('.cep-input').value;
+    const {
+      address,
+      street,
+      district,
+      neighborhood,
+      state,
+      city,
+      message,
+    } = await getAddress(cepInput);
+    if (message) {
+      cartAddressSpan.innerHTML = '';
+      cartAddressSpan.innerHTML = message;
+      throw new Error(message);
+    }
+    cartAddressSpan.innerHTML = '';
+    cartAddressSpan.innerHTML = `${address || street
+    } - ${district || neighborhood} - ${city} - ${state}`;
+  });
+}
